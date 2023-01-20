39ceff4631b0e835ec301ed20cd10b5430a4bb55
Murad-Hasan <muradhasan.7845@gmail.com>
2023-01-20 09:46:16 +0600

    binary_search

---
+++ Binary_search.js
@@ -0,0 +1,30 @@
+const array = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+];
+
+/**
+ * Searches recursively number from the list
+ * @param {Array} list Source array
+ * @param {number} item Search item
+ * @returns {(number|null)} Number if the value is found or NULL otherwise
+ */
+const binary_search = (list, item) => {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = list[mid];
+    if (guess === item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+};
+
+console.log(binary_search(array, 25));
+console.log(binary_search(array, 11));
