2cb6c58a932ce1083685604e0f8915300dabe2e0
Murad-Hasan <muradhasan.7845@gmail.com>
2023-01-25 02:46:21 +0600

    validate subsequence

---
+++ algorithms_practice/1. Easy/2.Validate_Subsequence.js
@@ -0,0 +1,21 @@
+const arr = [2, 1, 16, 9, 18, 9, 7, 11, 5, 3, 17];
+const subsequence = [1, 9, 11, 3];
+/**
+ * validate subsequence
+ * @param {Array} array array
+ * @param {Array} subsequence array
+ * @returns {boolean} boolean
+ */
+function validateSubsequence(array, subsequence) {
+  arrIdx = 0;
+  subIdx = 0;
+  while (arrIdx < array.length && subIdx < subsequence.length) {
+    if (array[arrIdx] === subsequence[subIdx]) {
+      subIdx += 1;
+    }
+    arrIdx += 1;
+  }
+  return subIdx == subsequence.length;
+}
+
+console.log(validateSubsequence(arr, subsequence));
