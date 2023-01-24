0e744b9a1e9ead6dc927332387ff5bcf1c32d93a
Murad-Hasan <muradhasan.7845@gmail.com>
2023-01-25 03:18:52 +0600

    validate subsequence

--- algorithms_practice/1. Easy/2.Validate_Subsequence.js
+++ algorithms_practice/1. Easy/2.Validate_Subsequence.js
@@ -6,16 +6,25 @@ const subsequence = [1, 9, 11, 3];
  * @param {Array} subsequence array
  * @returns {boolean} boolean
  */
+// function validateSubsequence(array, subsequence) {
+//   arrIdx = 0;
+//   subIdx = 0;
+//   while (arrIdx < array.length && subIdx < subsequence.length) {
+//     if (array[arrIdx] === subsequence[subIdx]) {
+//       subIdx += 1;
+//     }
+//     arrIdx += 1;
+//   }
+//   return subIdx == subsequence.length;
+// }
+
 function validateSubsequence(array, subsequence) {
-  arrIdx = 0;
-  subIdx = 0;
-  while (arrIdx < array.length && subIdx < subsequence.length) {
-    if (array[arrIdx] === subsequence[subIdx]) {
-      subIdx += 1;
-    }
-    arrIdx += 1;
+  let subIdx = 0;
+  for (const value of array) {
+    if (subIdx === subsequence.length) break;
+    if (subsequence[subIdx] === value) subIdx += 1;
   }
-  return subIdx == subsequence.length;
+  return subIdx === subsequence.length;
 }
 
 console.log(validateSubsequence(arr, subsequence));
